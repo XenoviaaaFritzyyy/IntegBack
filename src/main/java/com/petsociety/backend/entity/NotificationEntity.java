5bd092d98458277e2ef59a5f324da1c713d60df4
Borii1 <borinaga911@gmail.com>
2023-12-01 17:37:01 +0800

    Notification

---
+++ src/main/java/com/petsociety/backend/entity/NotificationEntity.java
@@ -0,0 +1,83 @@
+package com.petsociety.backend.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class NotificationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    private String message;
+    private LocalDateTime timestamp;
+    private boolean status; // false for unread, true for read
+
+
+
+    public NotificationEntity() {
+        super();
+    }
+
+
+    public NotificationEntity(Long id, UserEntity user, String message, LocalDateTime timestamp, boolean status) {
+        this.id = id;
+        this.user = user;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.status = status;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    
+}
